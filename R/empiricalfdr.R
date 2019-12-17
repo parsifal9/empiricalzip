76cce7ca0aedb3c2e0032fe2d5e9e9c56c40cd98
Dunne, Rob (Data61, Eveleigh ATP) <Rob.Dunne@data61.csiro.au>
2019-12-18 09:35:02 +1100

    changes to C2 constant

--- R/empiricalfdr.R
+++ R/empiricalfdr.R
@@ -23,64 +23,68 @@ empiricalfdr<-function(estimate, x, FDRLevel=0.1){
     
     for(i in 1:max(x)){
         pi[i] <- min(1, sum(count[1:(i+1)])/(M*f0(estimate[1,i],estimate[2,i],estimate[3,i],C=i)))
-        psi[1,i] <- length(x[which(x<=i)])*(-log(sum(count[1:(i+1)] + cf)/M)) + length(x[which(x>i)])*(-log(1 + cf - sum(count[1:(i+1)] + cf)/M))
-        psi[2,i] <- log(M)*length(x[which(x > i)]) - sum(count[(i + 2):(max(x) + 1)]*(log(count[(i + 2):(max(x) + 1)] + cf)))
+        psi[1,i] <- length(x[which(x<=i)])*(-log(sum(count[1:(i+1)] + cf)/M)) + length(x[which(x>i)])*(-log(1 + cf - sum(count[1:(i+1)] + cf)/M)) # $\hat{C}_1$ Equation 6 
+        psi[2,i] <- log(M)*length(x[which(x > i)]) - sum(count[(i + 2):(max(x) + 1)]*(log(count[(i + 2):(max(x) + 1)] + cf))) # $\hat{C}_2$ Equation 7 
         psi[2,max(x)] <- log(M)*length(x[which(x > i)]) - sum(count[max(x)]*(log(count[max(x)] + cf)))
         xi[i] <- length(x[which(x<=i)])*(-log(pi[i] + cf)) + length(x[which(x>i)])*(-log(1 + cf - pi[i]))
-        cut[i] <- psi[2,i] + negLogLh(x=x,estimate[1,i],estimate[2,i],estimate[3,i],C=i)
+        #cut[i] <- psi[2,i] + negLogLh(x=x,estimate[1,i],estimate[2,i],estimate[3,i],C=i)   # $\hat{C}_1$ Equation 6
+        cut[i] <- psi[2,i] + negLogLh(x=x,estimate[1,i],estimate[2,i],estimate[3,i],C=i)   # $\hat{C}_2$ Equation 7
+        
     }
     
-    CO <- min(which(diff(sign(diff(cut)))>0)+2, which.min(cut))
+    C1 <- min(which(diff(sign(diff(cut)))>0)+2, which.min(cut))         # $\hat{C}_1$ Equation 6
+
+    
     if(class(estimate)== "zigpEstimate"){
-        AD <- min(max(x),ceiling(max((CO + 1),estimate[2,CO]/(exp(estimate[3,CO] - 1) - estimate[3,CO]), log(length(x))/(estimate[3,CO] - 1 - log(estimate[3,CO])))))
+        AD <- min(max(x),ceiling(max((C1 + 1),estimate[2,C1]/(exp(estimate[3,C1] - 1) - estimate[3,C1]), log(length(x))/(estimate[3,C1] - 1 - log(estimate[3,C1])))))
         E<-rep(NA, 6)
-        E[1] <- estimate[1,CO]
-        E[2] <- estimate[2,CO]
-        E[3] <- estimate[3,CO]
-        E[4] <- pi[CO]
-        E[5] <- CO
+        E[1] <- estimate[1,C1]
+        E[2] <- estimate[2,C1]
+        E[3] <- estimate[3,C1]
+        E[4] <- pi[C1]
+        E[5] <- C1
         E[6] <- AD
     } else if (class(estimate)== "zipEstimate"){
-        AD <- min(max(x),ceiling(max((CO + 1),estimate[2,CO], log(length(x)))))
+        AD <- min(max(x),ceiling(max((C1 + 1),estimate[2,C1], log(length(x)))))
         E<-rep(NA, 6)
-        E[1] <- estimate[1,CO]
-        E[2] <- estimate[2,CO]
+        E[1] <- estimate[1,C1]
+        E[2] <- estimate[2,C1]
         E[3] <- NA
-        E[4] <- pi[CO]
-        E[5] <- CO
+        E[4] <- pi[C1]
+        E[5] <- C1
         E[6] <- AD
     }  else if (class(estimate)== "gpEstimate"){
-        AD <- min(max(x),ceiling(max((CO + 1),estimate[2,CO]/(exp(estimate[3,CO] - 1) - estimate[3,CO]), log(length(x))/(estimate[3,CO] - 1 - log(estimate[3,CO])))))
+        AD <- min(max(x),ceiling(max((C1 + 1),estimate[2,C1]/(exp(estimate[3,C1] - 1) - estimate[3,C1]), log(length(x))/(estimate[3,C1] - 1 - log(estimate[3,C1])))))
         
         E<-rep(NA, 6)
         E[1] <- NA
-        E[2] <- estimate[2,CO]
-        E[3] <- estimate[3,CO]
-        E[4] <- pi[CO]
-        E[5] <- CO
+        E[2] <- estimate[2,C1]
+        E[3] <- estimate[3,C1]
+        E[4] <- pi[C1]
+        E[5] <- C1
         E[6] <- AD
     }else if (class(estimate)== "pEstimate"){
-        AD <- min(max(x),ceiling(max((CO + 1),estimate[2,CO], log(length(x)))))
+        AD <- min(max(x),ceiling(max((C1 + 1),estimate[2,C1], log(length(x)))))
         
         E<-rep(NA, 6)
         E[1] <- NA
-        E[2] <- estimate[2,CO]
+        E[2] <- estimate[2,C1]
         E[3] <- NA
-        E[4] <- pi[CO]
-        E[5] <- CO
+        E[4] <- pi[C1]
+        E[5] <- C1
         E[6] <- AD
     }
 
     
     
-    LFDR <- pi[CO]*gpdMixture(x=sort(x),estimate[1,CO],estimate[2,CO],estimate[3,CO])*f12(sort(x),length(x))
+    LFDR <- pi[C1]*gpdMixture(x=sort(x),estimate[1,C1],estimate[2,C1],estimate[3,C1])*f12(sort(x),length(x))
     sortx<-sort(x[which(x < (AD))])
-    U1 <- pi[CO]*gpdMixture(x=sortx,estimate[1,CO],estimate[2,CO],estimate[3,CO])*f12(sortx,length(x))
-    sortx<-sort(x[which(x  <=  CO)])
-    U2 <- pi[CO]*gpdMixture(x=sortx,estimate[1,CO],estimate[2,CO],estimate[3,CO])*f12(sortx,length(x))
+    U1 <- pi[C1]*gpdMixture(x=sortx,estimate[1,C1],estimate[2,C1],estimate[3,C1])*f12(sortx,length(x))
+    sortx<-sort(x[which(x  <=  C1)])
+    U2 <- pi[C1]*gpdMixture(x=sortx,estimate[1,C1],estimate[2,C1],estimate[3,C1])*f12(sortx,length(x))
     R[1] <- sum(LFDR[!is.na(LFDR)] < FDRLevel)
     R[2] <- sum(U1[!is.na(U1)] < FDRLevel) - sum(U2[!is.na(U2)] < FDRLevel) + length(x[which(x > (AD-1))])
-    BH <- RealMBHFDR(x,estimate[1,CO],estimate[2,CO],estimate[3,CO], FDRLevel = FDRLevel, M=length(x), piZero=pi[CO], frequency=count[which(count > 0)])
+    BH <- RealMBHFDR(x,estimate[1,C1],estimate[2,C1],estimate[3,C1], FDRLevel = FDRLevel, M=length(x), piZero=pi[C1], frequency=count[which(count > 0)])
     Rejection <- cbind(R[1],R[2], BH)
     names(Rejection) <- c("One-stage procedure","Two-stage procedure","Storey’s FDR")
     names(E) <- c("eta","lambda","theta","pi","C1","D(C1)")
